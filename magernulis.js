48f241f2fc6752c889e05980c06cc6990443af72
MFarelS <58540890+MFarelS@users.noreply.github.com>
2020-11-23 11:19:34 +0700

    Mager Nulis BOT By MFarelS

--- magernulis.js
+++ magernulis.js
@@ -1,4 +1,4 @@
-module.exports = options = (headless, start) => {
+module.exports = magernulis = (headless, start) => {
     // const chromePath = {
     //     win32: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // Windows 32 bit
     //     win64: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', // Windows 64 bit
